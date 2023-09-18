d32bc72c8765b87c5044ee5b5d8ed699b77d2c55
Corelol12345 <76634545+Corelol12345@users.noreply.github.com>
2023-09-18 17:39:04 -0300

    Loop

--- Projeto.py
+++ Projeto.py
@@ -1,6 +1,27 @@
-print("-:: Sistema para Análise de Chuva de Meteoros ::-")
-print("1. Definir perímetro da propriedade e da edificação de interesse")
-print("2. Unificar sistemas de coordenadas de referência")
-print("3. Processar registros de chuva de meteoros")
-print("4. Apresentar estatísticas")
-print("5. Sair")
+while True:
+    print("-:: Sistema para Análise de Chuva de Meteoros ::-")
+    print("1. Definir perímetro da propriedade e da edificação de interesse")
+    print("2. Unificar sistemas de coordenadas de referência")
+    print("3. Processar registros de chuva de meteoros")
+    print("4. Apresentar estatísticas")
+    print("5. Sair")
+
+    op = int("Opção: ")
+    if op == 5:
+        break
+    if op == 1:
+        #entrada das cordenadas da fazenda
+        #x,y e x1,y1
+        #xprop,yptop e x1prop,y1prop
+    elif op == 2:
+        #entrada das cordenadas x da UPMCC e y da UPMCC
+    elif op == 3:
+        
+        while True:
+            #entrada de distancia e angulo da queda do meteorito
+            if distancia <0:
+                break
+                #conversao de coordenada
+                # verificar se o meteorito caiu na fazenda
+    elif op == 4:
+        #print das estatisticas com as contagens feitas no item 3
